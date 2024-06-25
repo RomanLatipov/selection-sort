c06267a023e48a08f16ae8ddba59a8168bb1f6c3
RomanLatipov <romanlatipov@gmail.com>
2024-06-25 15:14:45 -0400

    complete selection-sort lab

--- javascript/selection_sort.js
+++ javascript/selection_sort.js
@@ -1,5 +1,14 @@
 function selectionSort(arr) {
-  // type your code here
+  for (let i=0; i<arr.length-1; i++) {
+    for (let j=i+1; j<arr.length; j++) {
+      if (arr[i] > arr[j]) {
+        const temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+  return arr;
 }
 
 if (require.main === module) {
